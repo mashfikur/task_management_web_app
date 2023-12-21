a377417682069c8be2254241a9a0d6d129db5184
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-12-21 13:55:12 +0600

    navbar setup completed

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h3>welcome to navbar</h3>
+      <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
+        Open drawer
+      </label>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Drawer from "../components/drawer/Drawer";
+import Navbar from "../components/navbar/Navbar";
 
 const Layout = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Drawer>
+        <Outlet></Outlet>
+      </Drawer>
+    </div>
+  );
 };
 
 export default Layout;
