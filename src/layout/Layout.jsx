90721e9d8dbe6ba4f5e528d159a7826a155bcd7d
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-12-22 16:35:17 +0600

    firebassetup completed

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,16 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Drawer from "../components/drawer/Drawer";
 import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
 
 const Layout = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      <Drawer>
-        <div className=" container  mx-auto">
-          <Outlet></Outlet>
-        </div>
-      </Drawer>
+      <div className="main">
+        <Navbar></Navbar>
+        <Drawer>
+          <div className=" container  mx-auto">
+            <Outlet></Outlet>
+          </div>
+        </Drawer>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
