3d76292c1d397a4f8ce88870b0ba0184fe1cf242
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-12-24 16:19:20 +0600

    origin website updated

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const useAxiosSecure = () => {
   const axiosSecure = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://task-management-server-ruddy-kappa.vercel.app",
   });
 
   return axiosSecure;
